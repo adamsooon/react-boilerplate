a350557cbfa05ed34fcdbd757ca8c3e58227a963
Adamsooon <adamsooon@gmail.com>
2019-03-23 10:47:25 +0100

    css loader, postcss, autoprefixer

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const App = () => (
+    <div>
+        <p>adan</p>
+    </div>
+);
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-const title = 'My Minimal React Webpack Babel Setup';
+import App from './App';
+import './style.scss';
+import "@babel/polyfill";
 
 ReactDOM.render(
-  <div>{title}</div>,
-  document.getElementById('app')
+    <App/>,
+    document.getElementById('app')
 );
 
 module.hot.accept();
